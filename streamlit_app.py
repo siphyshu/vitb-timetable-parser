a1ad3417e9020ac16dd6533270ed92a0b48f4858
Jaiyank Saxena <siphyshu@gmail.com>
2024-05-05 11:18:01 +0530

    Added progress and status indicators

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,20 +24,26 @@ def main():
     st.markdown("""This is a demo web app that parses vitb timetables images to json/csv using [vitb-timetable-parser](https://github.com/siphyshu/vitb-timetable-parser).""")
     st.divider()
 
+    
     uploaded_file = st.file_uploader("Upload your timetable", type=["png", "jpeg"], accept_multiple_files=False, help="Upload a clear and high-quality __screenshot__ of the timetable. Make sure the timetable is not cropped or rotated.")
 
     if uploaded_file is not None:
+        # Display progress bar or success message in status container
+        status_container = st.empty()
+        progress_bar = status_container.progress(0)
+
         # Display uploaded image
-        st.write("Uploaded Timetable:")
         st.image(uploaded_file, use_column_width=True)
 
         # Parse timetable
-        parsed_timetable = parse_timetable(image=uploaded_file)
+        parsed_timetable = parse_timetable(image=uploaded_file, progress_callback=lambda progress: progress_bar.progress(progress))
+
+        # Show success message in status container
+        status_container.success("Timetable parsed successfully!")
 
         # Display parsed timetable
         st.write("Parsed Timetable:")
         st.dataframe(parsed_timetable)
-
 
 if __name__ == "__main__":
     main()
